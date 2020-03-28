90295b86ddb32c68f6a844e0579789405c00e8c2
Jorg van der Ham <jorgvanderham@Jorgs-MacBook-Pro.local>
2020-03-28 15:12:09 +0100

    added random route

--- class-example-project/app.js
+++ class-example-project/app.js
@@ -54,5 +54,8 @@ app.locals.title = 'Express - Generated with IronGenerator';
 const index = require('./routes/index');
 app.use('/', index);
 
+app.get('/someroute', (req, res, next) => {
+  res.send('this is some route')
+})
 
 module.exports = app;
